081f073d32eead2b214af21ba8d4f828f6078434
dens90 <dens90@hotmail.it>
2026-03-31 09:57:38 +0200

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/components-accordion-workshop', // Aggiungi questo
+  assetPrefix: '/components-accordion-workshop', // E questo
   images: {
     minimumCacheTTL: 60,
     unoptimized: true,
