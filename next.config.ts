843278d2e82058560c1efa3c386aca972ec1f053
dens90 <dens90@hotmail.it>
2026-03-31 10:27:15 +0200

    fix next js config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+// Controlliamo se siamo in produzione
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-
-  images: {
+  basePath: isProd ? '/components-accordion-workshop' : '',
+  assetPrefix: isProd ? '/components-accordion-workshop/' : '',  images: {
     minimumCacheTTL: 60,
     unoptimized: true,
   },
